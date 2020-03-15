3aeb6cc7010a8c66d9fff06c98be5eba989ed7af
Peter Jang <peter.x.jang@gmail.com>
2020-03-15 10:36:45 -0500

    Add signup action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
+    post "/users" => "users#create"
+
     get "/recipes" => "recipes#index"
     post "/recipes" => "recipes#create"
     get "/recipes/:id" => "recipes#show"
