6561875d73f08325b5234a0ecbc95b434b7b4110
Peter Jang <peter.x.jang@gmail.com>
2020-03-24 20:34:50 -0500

    Add HTML RESTful recipes create action, and links to index and show on all pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,12 +4,27 @@ class RecipesController < ApplicationController
     render "index.html.erb"
   end
 
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @recipe = Recipe.new(
+      title: params[:title],
+      chef: params[:chef],
+      prep_time: params[:prep_time],
+      ingredients: params[:ingredients],
+      directions: params[:directions],
+      image_url: params[:image_url],
+      user_id: 1,
+    )
+    @recipe.save
+    # render "show.html.erb"
+    redirect_to "/recipes"
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render "show.html.erb"
-  end
-
-  def new
-    render "new.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/recipes" => "recipes#index"
   get "/recipes/new" => "recipes#new"
-
+  post "/recipes" => "recipes#create"
   get "/recipes/:id" => "recipes#show"
 
   namespace :api do
