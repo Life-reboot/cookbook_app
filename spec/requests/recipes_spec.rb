7220178fb4bca1c94462f6b06b71b285c104c016
Peter Jang <peter.x.jang@gmail.com>
2020-04-28 18:46:03 -0500

    Add recipes update action test

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -49,4 +49,53 @@ RSpec.describe "Recipes", type: :request do
       expect(recipe["chef"]).to eq("example chef")
     end
   end
+
+  describe "PATCH /recipes/:id" do
+    it "updates a recipe from the given params" do
+      # Make fake data
+      user = User.create!(name: "Peter", email: "peter@example.com", password: "password")
+      recipe = Recipe.create!(title: "example title 1", chef: "example chef", ingredients: "example ingredients", directions: "...", image_url: "...", prep_time: 100, user_id: user.id)
+      jwt = JWT.encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+
+      # Make the web request
+      patch "/api/recipes/#{recipe.id}",
+        params: { "input_title" => "New title", "input_chef" => "New chef" },
+        headers: { "Authorization" => "Bearer #{jwt}" }
+      recipe = JSON.parse(response.body)
+
+      # Test the response
+      expect(response).to have_http_status(200)
+      expect(recipe["title"]).to eq("New title")
+    end
+
+    it "returns an error status code if no jwt provided" do
+      # Make fake data
+      user = User.create!(name: "Peter", email: "peter@example.com", password: "password")
+      recipe = Recipe.create!(title: "example title 1", chef: "example chef", ingredients: "example ingredients", directions: "...", image_url: "...", prep_time: 100, user_id: user.id)
+
+      # Make the web request
+      patch "/api/recipes/#{recipe.id}",
+        params: { "input_title" => "New title", "input_chef" => "New chef" }
+      recipe = JSON.parse(response.body)
+
+      # Test the response
+      expect(response).to have_http_status(401)
+    end
+
+    it "returns an error status code if invalid data" do
+      # Make fake data
+      user = User.create!(name: "Peter", email: "peter@example.com", password: "password")
+      recipe = Recipe.create!(title: "example title 1", chef: "example chef", ingredients: "example ingredients", directions: "...", image_url: "...", prep_time: 100, user_id: user.id)
+      jwt = JWT.encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.fetch(:secret_key_base), "HS256")
+
+      # Make the web request
+      patch "/api/recipes/#{recipe.id}",
+        params: { "input_title" => "New title", "input_chef" => "" },
+        headers: { "Authorization" => "Bearer #{jwt}" }
+      recipe = JSON.parse(response.body)
+
+      # Test the response
+      expect(response).to have_http_status(422)
+    end
+  end
 end
