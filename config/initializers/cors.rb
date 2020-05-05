6e8880e6c16fdb95ef86c29db1617f9baa661aa2
Peter Jang <peter.x.jang@gmail.com>
2020-05-05 18:32:36 -0500

    Add CORS for netlify frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "example.com"
+    origins "localhost:8080", "reverent-ritchie-828fd1.netlify.app"
 
     resource "*",
       headers: :any,
